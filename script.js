7be1ca55ce5ed818d06b467cc617905f5220af5e
boredrum <vvmaiboroda@gmail.com>
2023-03-23 14:45:04 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// let userAge = +prompt("What's your age?", 0);
+// if (userAge <= 17){
+//     alert("Do you study at school?")
+// }else if (userAge >17 && userAge < 25){
+//     alert("Do you study at university?")
+// }else if (userAge >= 25 && userAge < 60){
+//     alert("Do you have a job?")
+// }else{
+//     alert("What are you doing?")
+// }
+
+// let displayProperty = prompt('Please set display property', 'block, flex, grid, inline, none');
+// switch(displayProperty){
+//     case 'block':
+//         alert("Set 'display: block;'");
+//         break
+//     case 'flex':
+//         alert("Set 'display: flex;'");
+//         break
+//     case 'grid':
+//         alert("Set 'display: grid;'");
+//         break
+//     case 'inline':
+//         alert("Set 'display: inline;'");
+//         break
+//     case 'none':
+//         alert("Set 'display: none;'");
+//         break
+//     default:
+//         alert("Set 'display: inline-block;'");
+//         break
+// }
+
+// let num = Number(prompt('Please enter the number', 1));
+// while (!num || num <= 0){
+//     num = Number(prompt('Your number is incorrect, please enter correct number', 1));
+// }
+// alert(`${num} by 12 equals ${num*12}`);
+
+// for (i=1; i <= 50; i++){
+//     if(i%2 !== 0){
+//         continue
+//     }else{
+//         console.log(i);
+//     }
+// }
